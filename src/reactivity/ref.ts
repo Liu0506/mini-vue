36c1459a2412cdf4c789686a2b17b935e1375472
Wind <891820563@qq.com>
2022-05-31 13:47:44 +0800

    proxyRefs 功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,6 +1,6 @@
 import { hasChanged, isObject } from "../shared";
 import { isTracking, trackEffects, triggerEffects } from "./effect";
-import { reactive } from "./reactive";
+import { isReactive, reactive } from "./reactive";
 
 export function ref(value?: unknown) {
   return createRef(value);
@@ -55,6 +55,40 @@ export function isRef(ref) {
   return !!(ref && ref.__v_isRef === true);
 }
 
+/**
+ * 如果参数是 ref，则返回内部值，否则返回参数本身。
+ * 这是 val = isRef(val) ? val.value : val 计算的一个语法糖。
+ */
 export function unRef(ref) {
   return isRef(ref) ? ref.value : ref;
+}
+
+const shallowUnwrapHandlers = {
+  get(target, key) {
+    return unRef(Reflect.get(target, key));
+  },
+  set(target, key, value) {
+    const oldValue = target[key];
+    if (isRef(oldValue) && !isRef(value)) {
+      return (target[key].value = value);
+    } else {
+      return Reflect.set(target, key, value);
+    }
+  },
+};
+
+/**
+ * 这个函数的目的是
+ * 帮助解构 ref
+ * 比如在 template 中使用 ref 的时候，直接使用就可以了
+ * 例如： const count = ref(0) -> 在 template 中使用的话 可以直接 count
+ * 解决方案就是通过 proxy 来对 ref 做处理
+ */
+export function proxyRefs(objectWithRefs) {
+  // ref 中的 reactive 已处理，reactive 中的 ref 未处理，
+  // 后续会在 reactive中，判断 ref 类型，进行特殊处理
+  // 现在先不添加
+  return isReactive(objectWithRefs)
+    ? objectWithRefs
+    : new Proxy(objectWithRefs, shallowUnwrapHandlers);
 }
