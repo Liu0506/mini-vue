a3deab83dc34960fc83bced33e8ea914dcf255bb
Wind <891820563@qq.com>
2022-08-19 15:35:04 +0800

    实现初始化 element 主流程

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -9,9 +9,38 @@ function patch(vnode, container) {
   // 去处理组件
   // 判断 vnode 是不是一个 element
   if (typeof vnode.type === "string") {
-    // processElement();
+    processElement(vnode, container);
   } else {
     processComponent(vnode, container);
+  }
+}
+
+function processElement(vnode, container) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode, container) {
+  const el = document.createElement(vnode.type);
+  const { props, children } = vnode;
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    // 绑定子元素
+    mountChildren(children, el);
+  }
+
+  for (const propsKey in props) {
+    if (props.hasOwnProperty(propsKey)) {
+      const val = props[propsKey];
+      el.setAttribute(propsKey, val);
+    }
+  }
+  container.append(el);
+}
+
+function mountChildren(children: any[], el: any) {
+  for (const child of children) {
+    patch(child, el);
   }
 }
 
