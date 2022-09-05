dca378c50715a2d83cda53ae9c2bea9908770d2d
Liu0506 <31015664+Liu0506@users.noreply.github.com>
2022-09-05 14:36:55 +0800

    实现 shapeFlags，判断节点类型

---
+++ src/shared/shapeFlags.ts
@@ -0,0 +1,13 @@
+export const enum ShapeFlags {
+  ELEMENT = 1,                    // 00000001 元素节点
+  FUNCTIONAL_COMPONENT = 1 << 1,  // 00000010 函数组件
+  STATEFUL_COMPONENT = 1 << 2,    // 00000100 有状态组件
+  TEXT_CHILDREN = 1 << 3,         // 00001000 文本子节点
+  ARRAY_CHILDREN = 1 << 4,        // 00010000 数组子节点
+  SLOTS_CHILDREN = 1 << 5,
+  TELEPORT = 1 << 6,
+  SUSPENSE = 1 << 7,
+  COMPONENT_SHOULD_KEEP_ALIVE = 1 << 8,
+  COMPONENT_KEPT_ALIVE = 1 << 9,
+  COMPONENT = ShapeFlags.STATEFUL_COMPONENT | ShapeFlags.FUNCTIONAL_COMPONENT,
+}
